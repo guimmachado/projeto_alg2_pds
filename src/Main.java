56eca5c3a4cdc250bd52126a342d50bef30db7e0
Guilherme Machado <guimiguel.machado@gmail.com>
2025-03-11 21:28:59 -0300

    Class GerenciamentoProdutos created

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         Cliente c1 = new Cliente(1, "Guilherme", "Rua Jaci, 51", "06-08-2003");
+        Produtos p1 = new Produtos(1, "Batedeira", 399, 23, "Eletrodomesticos");
         GerenciamentoClientes listaClientes = new GerenciamentoClientes();
+        GerenciamentoProdutos listaProdutos = new GerenciamentoProdutos();
 
         listaClientes.adicionarCliente(c1);
         System.out.println(listaClientes.listarClientes());
+
+        listaProdutos.adicionarProdutos(p1);
+        System.out.println(listaProdutos.listarProdutos());
+
+
     }
 }
